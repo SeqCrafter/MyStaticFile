4b96a54d0e212e80512046cd31eae32f40576f45
XiaoHanYS <201495013@qq.com>
2026-06-08 14:41:40 +0800

    Refactor webstat.js to use async/await for fetch

--- myblog/webstat.js
+++ myblog/webstat.js
@@ -1,8 +1,10 @@
-(function () {
+(async function () {
   const websiteID = "5d9ec39a-2049-405e-8d8c-d3e811006836";
+  const shareID = "Cqwf1lUqMmC1a30H";
   const startAt = 0;
   const endAt = new Date().getTime();
 
+  const TOKEN_API = `https://cloud.umami.is/analytics/us/api/share/${shareID}`;
   const API_URL = `https://cloud.umami.is/analytics/us/api/websites/${websiteID}/stats?startAt=${startAt}&endAt=${endAt}`;
 
   const pvEl = document.getElementById("umami_value_site_pv");
@@ -10,27 +12,37 @@
 
   if (!pvEl || !vEl) return;
 
-  fetch(API_URL, {
-    headers: {
-      "x-umami-share-token":
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzaGFyZUlkIjoiNDcxOWVhZmYtNDYzNC00MTkyLWIwMmQtODlhNzlkMzM1N2RhIiwic2hhcmVUeXBlIjoxLCJwYXJhbWV0ZXJzIjp7Im92ZXJ2aWV3Ijp0cnVlfSwid2Vic2l0ZUlkIjoiNWQ5ZWMzOWEtMjA0OS00MDVlLThkOGMtZDNlODExMDA2ODM2IiwiaWF0IjoxNzgwMjc5MzQxfQ.-4A8cl__1JeTjmjWvvcm2dkJzEbllpvx2QIfY8prSjM",
-      "x-umami-share-context":
-        "1"
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      if (typeof data.pageviews === "number") {
-        console.log(data.pageviews);
-        pvEl.textContent = data.pageviews.toLocaleString();
-      }
-      if (typeof data.visitors === "number") {
-        console.log(data.visitors);
-        vEl.textContent = data.visitors.toLocaleString();
-      }
-    })
-    .catch(() => {
-      pvEl.textContent = "-";
-      vEl.textContent = "-";
+  let x_umami_share_token = "";
+
+  try {
+    const tokenRes = await fetch(TOKEN_API);
+    const tokenData = await tokenRes.json();
+    x_umami_share_token = tokenData.token;
+  } catch {
+    return;
+  }
+
+  if (!x_umami_share_token) return;
+
+  try {
+    const statsRes = await fetch(API_URL, {
+      headers: {
+        "x-umami-share-token": x_umami_share_token,
+        "x-umami-share-context": "1",
+      },
     });
+    const data = await statsRes.json();
+
+    if (typeof data.pageviews === "number") {
+      //console.log(data.pageviews);
+      pvEl.textContent = data.pageviews.toLocaleString();
+    }
+    if (typeof data.visitors === "number") {
+      //console.log(data.visitors);
+      vEl.textContent = data.visitors.toLocaleString();
+    }
+  } catch {
+    pvEl.textContent = "-";
+    vEl.textContent = "-";
+  }
 })();
